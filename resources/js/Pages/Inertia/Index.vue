bde990fffa9037a81ed87cf61cbb88176c4aeacb
matsu-taro <eee.rip179@gmail.com>
2024-02-05 15:49:11 +0900

    Link ルートパラメータまで

---
+++ resources/js/Pages/Inertia/Index.vue
@@ -0,0 +1,6 @@
+<script setup>
+
+</script>
+<template>
+  ああああああああ
+</template>
---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+  Inertiaテストです。<br>
+
+  <a href="/">aタグから</a>
+  <br>
+
+  <Link href="/">Linkコンポーネントから</Link>
+  <br>
+
+  <Link :href="route('inertia.index')">ルートの確認です</Link>
+  <br>
+
+  <Link :href="route('inertia.show', { id: 50 })">showルートです</Link>
+  <br>
+</template>
